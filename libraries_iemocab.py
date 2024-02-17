40fe798a67bab44363739646cb533af72ab263ef
Waelaburezeq <w.aburezeq@gmail.com>
2024-02-17 10:30:35 +0400

    Update libraries_iemocab.py

--- libraries_iemocab.py
+++ libraries_iemocab.py
@@ -1,9 +1,10 @@
+#%run -i libraries.py
 from __future__ import print_function
-###storage libraries
+#storage libraries
 import boto3
 import os
 import pickle
-###processing libraries
+#processing libraries
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -18,22 +19,28 @@ import patoolib
 from IPython.display import display, Javascript
 import warnings
 warnings.filterwarnings('ignore')
-###text libraries
+#text libraries
 import nltk
 nltk.download('punkt')
 from nltk.tokenize import word_tokenize
 from gensim.models import word2vec
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 import re
-###audio libraries 
+#audio libraries
 from scipy.io import wavfile
 import librosa.display
-###import scipy.io
-###Model libraries 
+#import scipy.io
+#Model libraries
 import tensorflow as tf
 import tensorflow.keras as keras
 from tensorflow.keras import Sequential
-from tensorflow.keras.layers import Dense, Embedding, Dropout, LSTM
+from tensorflow.keras.layers import Dense, Embedding, Dropout, LSTM,Input
+from tensorflow.keras.optimizers.legacy import Adam #decay was depreciated from recent adam optimizer
+from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.initializers import TruncatedNormal
+from tensorflow.keras.losses import CategoricalCrossentropy
+from tensorflow.keras.metrics import CategoricalAccuracy
+from tensorflow.keras.utils import to_categorical
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
@@ -41,17 +48,13 @@ from keras.utils import plot_model
 from keras.regularizers import l2
 from keras.callbacks import LearningRateScheduler
 import keras.backend as K
-import numpy as np
-import pandas as pd
 import torch
 import torch.nn.functional as F
 import matplotlib.pyplot as plt
 import seaborn as sns
-import numpy as np
 import IPython.display as ipd
 import seaborn as sns
 import warnings
-import os
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -60,3 +63,4 @@ from torch.utils.data import Dataset, DataLoader
 from sklearn.metrics import confusion_matrix, classification_report
 from torch import nn, optim
 from torchvision import transforms, models, datasets
+from helper_prabowo_ml import clean_html, remove_links, non_ascii, lower, email_address, removeStopWords, punct, remove_
