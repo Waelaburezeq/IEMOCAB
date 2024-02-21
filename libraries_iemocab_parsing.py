660f6dfc40040bef3223c9698cf7d3056f05db05
Waelaburezeq <w.aburezeq@gmail.com>
2024-02-21 19:44:47 +0400

    Create libraries_iemocab_parsing.py

---
+++ libraries_iemocab_parsing.py
@@ -0,0 +1,16 @@
+import glob
+import pandas as pd
+import pickle
+import os
+import numpy as np
+import warnings
+warnings.filterwarnings("ignore")
+import joblib
+import librosa
+from operator import itemgetter
+import matplotlib.pyplot as plt
+import librosa.display
+import multiprocessing as mp
+import tqdm
+import shutil
+from tqdm import tqdm
