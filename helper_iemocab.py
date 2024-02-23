b087ed9ff87973fa4ecbaaea2f1865ddbd601102
Waelaburezeq <w.aburezeq@gmail.com>
2024-02-23 06:09:22 +0400

    Update helper_iemocab.py

--- helper_iemocab.py
+++ helper_iemocab.py
@@ -1,4 +1,5 @@
 #from helper_iemocab import *
+import boto3
 
 def func_args(obj:"FunctionName")->"This function returns all input args and thier annotation":
   return inspect.getfullargspec(load_files)
